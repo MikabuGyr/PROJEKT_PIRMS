d97a08277dc076c494a0f5b9c2ccf1dc72ee41e8
Alex123CZ <34350689+Alex123CZ@users.noreply.github.com>
2025-05-07 22:43:51 +0200

    Add files via upload

---
+++ Projekt/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Projekt
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comboComPort;
+        private System.Windows.Forms.ComboBox comboBaudRate;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartFFT;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboComPort = new System.Windows.Forms.ComboBox();
+            this.comboBaudRate = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.chartFFT = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartFFT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboComPort
+            // 
+            this.comboComPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboComPort.FormattingEnabled = true;
+            this.comboComPort.Location = new System.Drawing.Point(20, 20);
+            this.comboComPort.Name = "comboComPort";
+            this.comboComPort.Size = new System.Drawing.Size(100, 21);
+            this.comboComPort.TabIndex = 0;
+            // 
+            // comboBaudRate
+            // 
+            this.comboBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBaudRate.FormattingEnabled = true;
+            this.comboBaudRate.Location = new System.Drawing.Point(130, 20);
+            this.comboBaudRate.Name = "comboBaudRate";
+            this.comboBaudRate.Size = new System.Drawing.Size(100, 21);
+            this.comboBaudRate.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(250, 18);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(335, 18);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 25);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(20, 50);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 4;
+            // 
+            // chartFFT
+            // 
+            this.chartFFT.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.Maximum = 5000D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.Title = "Frekvence [Hz]";
+            chartArea1.AxisY.Maximum = 600D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.Title = "Amplituda";
+            chartArea1.Name = "ChartArea1";
+            this.chartFFT.ChartAreas.Add(chartArea1);
+            this.chartFFT.Location = new System.Drawing.Point(20, 70);
+            this.chartFFT.Name = "chartFFT";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Name = "Spektrum";
+            this.chartFFT.Series.Add(series1);
+            this.chartFFT.Size = new System.Drawing.Size(760, 380);
+            this.chartFFT.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.comboComPort);
+            this.Controls.Add(this.comboBaudRate);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.chartFFT);
+            this.Name = "Form1";
+            this.Text = "FFT Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.chartFFT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Projekt/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private List<double> sampleBuffer = new List<double>();
+        private const int FFT_SIZE = 1024;
+        private const int MAX_SAMPLE = 255; // 8bitová data
+        private const int SAMPLE_RATE = 8000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBaudRate.Items.AddRange(new object[] { "9600", "19200", "38400", "57600", "115200" });
+            comboBaudRate.SelectedItem = "115200";
+
+            comboComPort.Items.AddRange(SerialPort.GetPortNames());
+            if (comboComPort.Items.Count > 0)
+                comboComPort.SelectedIndex = 0;
+
+            chartFFT.Series[0].Points.Clear();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string portName = comboComPort.SelectedItem?.ToString();
+            int baudRate = int.Parse(comboBaudRate.SelectedItem.ToString());
+
+            serialPort = new SerialPort(portName, baudRate);
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.ReadBufferSize = 4096;
+            serialPort.ReceivedBytesThreshold = 1;
+
+            try
+            {
+                serialPort.Open();
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                lblStatus.Text = $"Čtení z {portName} @ {baudRate}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při otevírání portu: " + ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort?.Close();
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                lblStatus.Text = "Odpojeno";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při zavírání portu: " + ex.Message);
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                int bytesToRead = serialPort.BytesToRead;
+                byte[] buffer = new byte[bytesToRead];
+                serialPort.Read(buffer, 0, bytesToRead);
+
+                lock (sampleBuffer)
+                {
+                    foreach (byte b in buffer)
+                    {
+                        double normalized = b / (double)MAX_SAMPLE;
+                        sampleBuffer.Add(normalized);
+                    }
+
+                    while (sampleBuffer.Count >= FFT_SIZE)
+                    {
+                        var data = sampleBuffer.Take(FFT_SIZE).ToArray();
+                        sampleBuffer.RemoveRange(0, FFT_SIZE);
+
+                        Task.Run(() => ComputeAndDisplayFFT(data));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() =>
+                {
+                    lblStatus.Text = $"Chyba při čtení: {ex.Message}";
+                }));
+            }
+        }
+
+        private void ComputeAndDisplayFFT(double[] samples)
+        {
+            Complex[] fftInput = samples.Select(v => new Complex(v, 0)).ToArray();
+            Fourier.Forward(fftInput, FourierOptions.Matlab);
+
+            double[] magnitudes = fftInput
+                .Take(FFT_SIZE / 2)
+                .Select(c => c.Magnitude)
+                .ToArray();
+
+            double[] freqs = Enumerable.Range(0, FFT_SIZE / 2)
+                .Select(i => i * SAMPLE_RATE / (double)FFT_SIZE)
+                .ToArray();
+
+            Invoke(new Action(() =>
+            {
+                chartFFT.Series[0].Points.Clear();
+                for (int i = 0; i < freqs.Length; i++)
+                {
+                    chartFFT.Series[0].Points.AddXY(freqs[i], magnitudes[i]);
+                }
+            }));
+        }
+    }
+}
